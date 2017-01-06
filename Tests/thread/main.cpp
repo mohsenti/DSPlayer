47f338577516c6b89791f437a7d46d1e80ae7f04
mohsen <mohsen.timar@gmail.com>
2017-01-06 11:38:06 +0330

    cast void* to char* to avoiding warning.

--- Tests/thread/main.cpp
+++ Tests/thread/main.cpp
@@ -13,7 +13,7 @@ std::condition_variable cv;
 bool ready = false;
 
 void cleanup(void *mem) {
-    delete mem;
+    delete (char *) mem;
 }
 
 void bar() {
@@ -23,9 +23,9 @@ void bar() {
         pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, nullptr);
         pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, nullptr);
         cv.notify_all();
-        while (true);
-        delete x;
-        pthread_exit(nullptr);
+        while (true); // Endless while
+//        delete x;
+//        pthread_exit(nullptr);
     pthread_cleanup_pop(0);
 }
 
