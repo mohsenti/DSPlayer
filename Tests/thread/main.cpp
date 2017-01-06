0d125b9ecb05a9ad74707afbba2d06a3fbc32f8b
mohsen <mohsen.timar@gmail.com>
2017-01-06 11:29:43 +0330

    add cleanup procedure to thread

--- Tests/thread/main.cpp
+++ Tests/thread/main.cpp
@@ -12,19 +12,27 @@ std::condition_variable cv;
 
 bool ready = false;
 
+void cleanup(void *mem) {
+    delete mem;
+}
+
 void bar() {
     ready = true;
     char *x = new char[INT32_MAX]{0};
-    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, nullptr);
-    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, nullptr);
-//    std::this_thread::sleep_for(std::chrono::seconds(5));
-    cv.notify_all();
-    while (true);
+    pthread_cleanup_push(cleanup, x);
+        pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, nullptr);
+        pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, nullptr);
+        cv.notify_all();
+        while (true);
+        delete x;
+        pthread_exit(nullptr);
+    pthread_cleanup_pop(0);
 }
 
 void foo() {
     std::thread *thread2 = new std::thread(bar);
     std::unique_lock<std::mutex> lck(mutex);
+    pthread_detach(thread2->native_handle());
     pthread_cancel(thread2->native_handle());
     std::this_thread::sleep_for(std::chrono::seconds(10));
 }
