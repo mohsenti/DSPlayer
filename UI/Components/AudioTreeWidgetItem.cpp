2833b0a743474b515fd4745d38f2aba4a7cb60cd
mohsen <mohsen.timar@gmail.com>
2017-01-17 16:54:09 +0330

    fix MediaInfo init before pthreads init problem

--- UI/Components/AudioTreeWidgetItem.cpp
+++ UI/Components/AudioTreeWidgetItem.cpp
@@ -8,29 +8,33 @@
 #include "MediaInfo.h"
 
 using namespace MediaInfoLib;
+
 namespace {
-    static MediaInfo MI;
+    Core::Ref<MediaInfo> MI(nullptr);
 }
 
 namespace UI {
     AudioTreeWidgetItem::AudioTreeWidgetItem(const QString &fileName) : QTreeWidgetItem() {
         this->filename = fileName;
-        MI.Open(this->filename.toStdWString());
+        if (!MI.IsValid()) {
+            MI.resetObject(new MediaInfo());
+        }
+        MI->Open(this->filename.toStdWString());
 
         bool dummy;
         setText(0, QString::fromStdWString(
-                MI.Get(MediaInfoLib::Stream_General, 0, __T("Title"), MediaInfoLib::Info_Text,
-                       MediaInfoLib::Info_Name)));
+                MI->Get(MediaInfoLib::Stream_General, 0, __T("Title"), MediaInfoLib::Info_Text,
+                        MediaInfoLib::Info_Name)));
         setText(1, Core::formatSecondsToTime(
                 QString::fromStdWString(
-                        MI.Get(MediaInfoLib::Stream_General, 0, __T("Duration"),
-                               MediaInfoLib::Info_Text,
-                               MediaInfoLib::Info_Name)).toInt(&dummy) / 1000).c_str());
+                        MI->Get(MediaInfoLib::Stream_General, 0, __T("Duration"),
+                                MediaInfoLib::Info_Text,
+                                MediaInfoLib::Info_Name)).toInt(&dummy) / 1000).c_str());
 
-        setText(2, QString::fromStdWString(MI.Get(MediaInfoLib::Stream_General, 0, __T("Album"),
-                                                  MediaInfoLib::Info_Text,
-                                                  MediaInfoLib::Info_Name)));
+        setText(2, QString::fromStdWString(MI->Get(MediaInfoLib::Stream_General, 0, __T("Album"),
+                                                   MediaInfoLib::Info_Text,
+                                                   MediaInfoLib::Info_Name)));
 
-        MI.Close();
+        MI->Close();
     }
 }
