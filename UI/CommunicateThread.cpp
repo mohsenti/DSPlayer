33e2ee646242a6c95aa3bb11146354f1a06b6305
mohsen <mohsen.timar@gmail.com>
2017-01-23 16:26:00 +0330

    Add Communicate thread class

---
+++ UI/CommunicateThread.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mohsen on 1/23/17.
+//
+
+#include "CommunicateThread.h"
+
+void CommunicateThread::run() {
+
+}
---
+++ UI/CommunicateThread.h
@@ -0,0 +1,19 @@
+//
+// Created by mohsen on 1/23/17.
+//
+
+#ifndef DSPLAYER_COMMUNICATETHREAD_H
+#define DSPLAYER_COMMUNICATETHREAD_H
+
+
+#include <QtCore/QThread>
+
+class CommunicateThread {
+protected:
+    virtual void run();
+public:
+
+};
+
+
+#endif //DSPLAYER_COMMUNICATETHREAD_H
