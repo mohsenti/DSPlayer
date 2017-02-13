7c853e2e3be03c8d5ed5045defef24c329c81c31
mohsen <mohsen.timar@gmail.com>
2017-02-13 10:39:51 +0330

    Create pipe to communicate between processes

--- UI/CommunicateThread.cpp
+++ UI/CommunicateThread.cpp
@@ -3,7 +3,49 @@
 //
 
 #include "CommunicateThread.h"
+#include <QtDebug>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <zconf.h>
+#include <sys/stat.h>
+
+#ifdef __linux__
 
 void CommunicateThread::run() {
 
 }
+
+void CommunicateThread::start() {
+    if (serverIsRunning()) {
+        fd = open(fileName.c_str(),O_WRONLY);
+        flock(fd, LOCK_SH | LOCK_NB);
+    } else {
+        if (!Core::fileExists(fileName)) {
+            if (mkfifo(fileName.c_str(), 438) != 0) {
+                //Todo:throw exception here, failed to create pipe
+            }
+        }
+        fd = open(fileName.c_str(), O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+        flock(fd, LOCK_SH | LOCK_NB);
+    }
+}
+
+void CommunicateThread::stop() {
+    close(fd);
+}
+
+bool CommunicateThread::serverIsRunning() {
+    if (!Core::fileExists(fileName))
+        return false;
+    int fd = open(fileName.c_str(), O_WRONLY | O_NONBLOCK);
+    if (fd == -1) {
+        //delete orphaned pipe
+        unlink(fileName.c_str());
+        return false;
+    } else {
+        close(fd);
+        return true;
+    }
+}
+
+#endif
--- UI/CommunicateThread.h
+++ UI/CommunicateThread.h
@@ -5,14 +5,25 @@
 #ifndef DSPLAYER_COMMUNICATETHREAD_H
 #define DSPLAYER_COMMUNICATETHREAD_H
 
-
 #include <QtCore/QThread>
+#include <fstream>
+#include "Core/Core.h"
+
+using namespace std;
 
 class CommunicateThread {
+private:
+    int fd;
+    string fileName;
 protected:
     virtual void run();
-public:
 
+public:
+    void start();
+
+    bool serverIsRunning();
+
+    void stop();
 };
 
 
