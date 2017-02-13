31b56f8a3ecdf2164f910fee6bce0f8068d6b0a7
mohsen <mohsen.timar@gmail.com>
2017-02-13 09:40:03 +0330

    Add getTmpDirectory translate from freepascal GetTmpDir

---
+++ Core/CoreFileSystem.cpp
@@ -0,0 +1,35 @@
+//
+// Created by mohsen on 2/13/17.
+//
+
+#include "CoreFileSystem.h"
+#include "Core.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <tchar.h>
+#endif
+
+string Core::getTmpDirectory() {
+    string result;
+#ifdef __linux__
+    result = Core::getEnvVariable("TEMP");
+    if (result.empty()) {
+        result = Core::getEnvVariable("TMP");
+        if (result.empty()) {
+            result = Core::getEnvVariable("TMPDIR");
+            if (result.empty()) {
+                result = "/tmp/";
+            }
+        }
+    }
+    if (result.at(result.length() - 1) != '/')
+        result += "/";
+#elif _WIN32
+    TCHAR lpTempPathBuffer[MAX_PATH];
+    DWORD dwRetVal = GetTempPath(MAX_PATH, lpTempPathBuffer); // buffer for path
+    result = lpTempPathBuffer;
+    if (result.at(result.length() - 1) != '\\')
+        result += "\\";
+#endif
+}
---
+++ Core/CoreFileSystem.h
@@ -0,0 +1,13 @@
+//
+// Created by mohsen on 2/13/17.
+//
+
+#ifndef DSPLAYER_COREFILESYSTEM_H
+#define DSPLAYER_COREFILESYSTEM_H
+#include <string>
+using namespace std;
+
+namespace Core {
+    string getTmpDirectory();
+}
+#endif //DSPLAYER_COREFILESYSTEM_H
